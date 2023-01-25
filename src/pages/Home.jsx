4a92eb8b353e6775ce406f8314d2c453aba0a573
devemit <i.mitko@outlook.com>
2023-01-25 21:24:54 +0100

    Change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,10 +8,10 @@ const Home = () => {
         <div className='max-h-[500px] relative'>
           <div className='absolute w-full h-full text-gray-200 max-h-[500px] flex flex-col justify-center'>
             <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-              The <span className='text-orange-500'>Future of</span>
+              The <span className='text-violet-600'>Future of</span>
             </h1>
             <h1 className='px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold'>
-              <span className='text-orange-500'>Commerce is</span> Yours
+              <span className='text-violet-600'>Commerce is</span> Yours
             </h1>
             <Link className='px-4 py-8' to='/shop'>
               <span className='py-4 px-10 md:py-6 md:px-14 bg-violet-300 text-black font-bold uppercase rounded-xl'>
@@ -20,8 +20,8 @@ const Home = () => {
             </Link>
           </div>
           <img
-            className='w-full h-screen object-cover'
-            src='https://images.unsplash.com/photo-1605902711622-cfb43c4437b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80'
+            className='w-full h-full object-cover'
+            src='https://images.pexels.com/photos/34577/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
             alt='/'
           />
         </div>
